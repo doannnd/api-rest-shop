a5d7575c7897def9d1d4011168721c6936fb66e0
doannd <nguyendinhdoanjdk@gmail.com>
2019-02-28 22:17:04 +0700

    hanndle error and improving the project settup

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const morgan = require('morgan');
+var chalk = require('chalk');
 
 const productRoute = require('./api/routes/product.route');
 const orderRoute = require('./api/routes/order.route');
@@ -6,15 +8,26 @@ const orderRoute = require('./api/routes/order.route');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        "message": "It worked"
-    });
-});
+app.use(morgan('tiny'));
 
 app.use('/product', productRoute);
 app.use('/order', orderRoute);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        "error": {
+            "message": error.message
+        }
+    })
+});
+
 app.listen(port, () => {
-    console.log('Server is running on port ', port);
+    console.log(chalk.blue('Server is running on port ', port));
 });
